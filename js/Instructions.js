39ec9bcbbefdc8da5492e45433e48b28183c8daa
Johnny Karasinski <karasinski@gmail.com>
2014-08-12 10:37:32 -0700

    update instructions state

--- js/Instructions.js
+++ js/Instructions.js
@@ -2,11 +2,14 @@ BoilerGame.Instructions = function(game) {};
 
 BoilerGame.Instructions.prototype = {
   create: function() {
-    this.game.stage.backgroundColor = '#000';
-    start_x = this.game.width - 100;
-    start_y = 50;
-    backButton = this.add.button(start_x, start_y,
-                                 'btnExit', this.returnToMenu, this, 0, 1, 2);
+    this.game.stage.backgroundColor = '#444';
+
+    x = 270, y = 390;
+    backButton = this.game.add.button(x, y, 'box', this.returnToMenu, this);
+    backButton.scale.setTo(1.5, 0.5);
+
+    backString = "Back"
+    backText = this.game.add.bitmapText(x + 45, y + 15, 'kenpixelblocks', backString, 20);
 
     this.drawInstructions();
   },
@@ -18,9 +21,21 @@ BoilerGame.Instructions.prototype = {
 
   // draw a pretty shitty Instructions
   drawInstructions: function() {
-    text = 'instructions go here'
+    text = 'Instructions'
+    style = {
+      font: "65px Arial",
+      fill: "#CCC",
+      align: "center"
+    };
+    t = this.game.add.text(this.game.world.centerX - 200, 50, text, style);
 
-    style = { font: "65px Arial", fill: "#ff0044", align: "center" };
-    t = this.game.add.text(this.game.world.centerX-200, 0, text, style);
+    text = 'This is you.\n\nYour goal in life is to jump as much as you can. \n\nYou can accomplish this marvelous feat by pressing \nthe spacebar or tapping the screen.'
+    style = {
+      font: "25px Arial",
+      fill: "#CCC",
+      align: "left"
+    };
+    t = this.game.add.text(this.game.world.centerX - 200, 200, text, style);
+    this.game.add.sprite(180, 160, 'hero', 1);
   }
 };
